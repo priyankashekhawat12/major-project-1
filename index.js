926b1915af835770102880f8ac5f586565e97518
priyanka Shekhawat <104977818+priyankashekhawat12@users.noreply.github.com>
2023-04-06 23:01:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const app = express();
-port = 8000;
-const expressLayout = require('express-ejs-layouts');
-app.use(expressLayout)
-const db = require('./config/mongoose')
-//const TodoLists = require('./models/todo_list')
-// it is use to handle middle ware here we are using express.urlenceode to us e the parser
-app.use(express.urlencoded()) 
-app.use('/',require('./routes'))
-app.use(express.static('./assets')) // for getting static
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true)
-app.set('view engine','ejs');
-app.set('views','./views')
-app.listen(port,function(err){
-    if(err){
-        console.log(`error in running the ${port}`)
-        return;
-    }
-    console.log(`Server is running @ ${port}`)
-})
+const router = express.Router();
+const homeController = require('../controller/homecontroller');
+router.get('/',homeController.home)
+router.post('/create_todo',homeController.createTodo) //controller for creating todo list
+router.post('/delete_todo',homeController.deleteTodo) // controller for deleting the todo list
+router.get('/editdata',homeController.EditPage)       // controller for getting Edit page
+router.post('/edit-todolist',homeController.editDetails) // conteoller for Edting todo list
+console.log('router is loaded')
+module.exports = router;
+
